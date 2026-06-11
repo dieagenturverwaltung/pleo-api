3b3204cce0042e4d25b72a7a371bccb59646cf43
MarlinKuhn <marlin.kuhn@web.de>
2026-06-11 11:31:30 +0200

    Added webhook payload structure

--- webhook/types.go
+++ webhook/types.go
@@ -46,3 +46,45 @@ type Activity struct {
 type Secret struct {
 	SecretKey string `json:"secretKey"`
 }
+
+type IPayload interface {
+	PayloadExport | PayloadVendor
+}
+
+// Payload is the payload submitted by pleo to the webhook url
+type Payload[T IPayload] struct {
+	WebhookId        string `json:"webhook-id"`
+	WebhookTimestamp int    `json:"webhook-timestamp"`
+	WebhookSignature string `json:"webhook-signature"`
+	Payload          struct {
+		Data      T      `json:"data"`
+		EventId   string `json:"eventId"`
+		EventType string `json:"eventType"`
+	} `json:"payload"`
+}
+
+type PayloadExport struct {
+	CompanyId     string    `json:"companyId"`
+	CreatedAt     time.Time `json:"createdAt"`
+	CreatedBy     string    `json:"createdBy"`
+	ExpiresIn     int       `json:"expiresIn"`
+	Id            string    `json:"id"`
+	NumberOfItems int       `json:"numberOfItems"`
+	Status        string    `json:"status"`
+}
+
+type PayloadVendor struct {
+	Id                    string      `json:"id"`
+	CompanyId             string      `json:"companyId"`
+	Name                  string      `json:"name"`
+	Country               string      `json:"country"`
+	RegistrationNumber    string      `json:"registrationNumber"`
+	TaxRegistrationNumber string      `json:"taxRegistrationNumber"`
+	Code                  interface{} `json:"code"`
+	State                 string      `json:"state"`
+	DefaultCurrency       string      `json:"defaultCurrency"`
+	CreatedBy             string      `json:"createdBy"`
+	CreatedAt             time.Time   `json:"createdAt"`
+	UpdatedAt             time.Time   `json:"updatedAt"`
+	DeletedAt             interface{} `json:"deletedAt"`
+}
